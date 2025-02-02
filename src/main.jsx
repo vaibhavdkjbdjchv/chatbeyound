b7cc8c7976b39d071488430628118bc8ef4f90fa
vaibhavdkjbdjchv <118902119+vaibhavdkjbdjchv@users.noreply.github.com>
2025-02-02 19:07:38 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom/client";
 import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App";
 
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-
-
-const domain = "dev-b4puqduaqy2fbl41.us.auth0.com"; // Your Auth0 domain
-const clientId = "42v7dqcFlUfr7owHNNhvs4eRvOhYbXW7"; // Your Auth0 Client ID
-const audience = `https://${domain}/api/v2/`; // Needed for API calls
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Auth0Provider
